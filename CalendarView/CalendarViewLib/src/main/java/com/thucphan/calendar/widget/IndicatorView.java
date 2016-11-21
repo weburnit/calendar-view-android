68f93d55bb92b74951af0798241eab4aa22af511
Thuc Phan <shinta.bmt@gmail.com>
2016-11-21 15:51:09 +0700

    add calendar view and example

---
+++ CalendarView/CalendarViewLib/src/main/java/com/thucphan/calendar/widget/IndicatorView.java
@@ -0,0 +1,11 @@
+package com.thucphan.calendar.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class IndicatorView extends WeekView {
+    public IndicatorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ CalendarView/app/src/main/java/thucphan/calendarview/ScrollingCalendarBehavior.java
@@ -0,0 +1,23 @@
+package thucphan.calendarview;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by shintabmt on 11/21/2016.
+ */
+
+public class ScrollingCalendarBehavior extends AppBarLayout.Behavior {
+
+    public ScrollingCalendarBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
+        return false;/*super.onInterceptTouchEvent(parent, child, ev);*/
+    }
+}
